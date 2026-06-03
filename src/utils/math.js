65f68096faa1ea5e215697b9afb0ce905f411e5b
Meher_Codez <156418981+MeherBamrah@users.noreply.github.com>
2026-06-03 13:45:02 +0530

    Create math.js

---
+++ src/utils/math.js
@@ -0,0 +1,15 @@
+// src/utils/math.js
+export const cosineSimilarity = (vecA, vecB) => {
+  let dotProduct = 0.0;
+  let normA = 0.0;
+  let normB = 0.0;
+  
+  for (let i = 0; i < vecA.length; i++) {
+    dotProduct += vecA[i] * vecB[i];
+    normA += Math.pow(vecA[i], 2);
+    normB += Math.pow(vecB[i], 2);
+  }
+  
+  if (normA === 0 || normB === 0) return 0;
+  return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+};
